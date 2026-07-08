e35f24721c656e53430fe200a03be35ff46138f3
winkwink-backend <rosariolettera62@gmail.com>
2026-07-08 17:36:17 +0200

    main patch provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { ColorProvider } from "./providers/ColorProvider"; // file aggiunto
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
