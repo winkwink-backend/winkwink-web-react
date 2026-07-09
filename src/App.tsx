f7bb56954f00ab47521593e840e7038743d15235
winkwink-backend <rosariolettera62@gmail.com>
2026-07-09 12:47:22 +0200

    app file  patchato

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import LoginPage from "./pages/LoginPage"
-import HomePage from "./pages/HomePage"
-// importa qui tutte le altre pagine che hai creato
+import AppRouter from "./routes/AppRouter";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* ⭐ La pagina che deve aprirsi sul dominio */}
-        <Route path="/" element={<LoginPage />} />
-
-        {/* ⭐ Le altre pagine */}
-        <Route path="/home" element={<HomePage />} />
-        {/* aggiungi qui tutte le altre route */}
-      </Routes>
-    </BrowserRouter>
-  )
+export default function App() {
+  return <AppRouter />;
 }
-export default App
